da1dd460cf0c115449b0df7fd518d7e28b1ae02f
frostbean <ylai88193@gmail.com>
2024-06-20 02:24:37 -0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+sudo apt install -y  build-essential
+sudo apt install -y  git
+sudo apt install -y  m4
+sudo apt install -y  scons
+sudo apt install -y  zlib1g
+sudo apt install -y zlib1g-dev
+sudo apt install -y libprotobuf-dev
+sudo apt install -y protobuf-compiler
+sudo apt install -y libprotoc-dev
+sudo apt install -y libgoogle-perftools-dev
+sudo apt install -y python3-dev
+sudo apt install -y python3-six
+sudo apt install -y python
+sudo apt install -y libboost-all-dev
+sudo apt install -y pkg-config
